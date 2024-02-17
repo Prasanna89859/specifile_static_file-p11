8829b468b7957427147889c5efd1e6c854064061
prasanna89859 <mprasanna015@gmail.com>
2024-02-17 11:25:10 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def kohli(request):
+    return render (request,'kohli.html')
+
+def kohli2(request):
+    return render(request,'kohli2.html')
+
